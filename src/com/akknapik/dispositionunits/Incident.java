f27f53814404d09314649b6742996ce5e7b621c0
Arkadiusz Knapik <arkadiusz.knapik.200@gmail.com>
2024-12-08 18:13:28 +0100

    Update Incident and Main

--- src/com/akknapik/dispositionunits/Incident.java
+++ src/com/akknapik/dispositionunits/Incident.java
@@ -4,6 +4,8 @@ import com.akknapik.dispositionunits.Strategy.DispatchStrategy;
 import com.akknapik.dispositionunits.Strategy.FireStrategy;
 import com.akknapik.dispositionunits.Strategy.LocalThreatStrategy;
 
+import java.util.Random;
+
 public class Incident {
     private final String nameOfIncident;
     private final Position position;
@@ -14,6 +16,15 @@ public class Incident {
     private final long timeToReturn;
     private boolean falseAlarmLogged = false;
 
+    public Incident() {
+        this.nameOfIncident = new Random().nextDouble() < 0.3 ? "Pożar" : "Miejscowe zagrożenie";
+        this.position = new Position(50.05 + new Random().nextDouble() * 0.1, 19.93 + new Random().nextDouble() * 0.1);
+        this.falseAlarm = new Random().nextDouble() < 0.05;
+        this.strategy = nameOfIncident.equals("Pożar") ? new FireStrategy() : new LocalThreatStrategy();
+        this.timeToArrive = (long) (Math.random() * 3000);
+        this.actionTime = this.falseAlarm ? 0 : (5000 + (long) (Math.random() * 20000));
+        this.timeToReturn = this.timeToArrive;
+    }
 
 
     public Incident(String nameOfIncident, Position position, boolean falseAlarm) {
